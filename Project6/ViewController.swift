785517ee10d23045a4e50239acdbd3d4b93ee160
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-06 14:30:02 +0900

    made the height of the labels 1/5th of the main view, minus 10 for the spacing

--- Project6/ViewController.swift
+++ Project6/ViewController.swift
@@ -62,10 +62,11 @@ class ViewController: UIViewController {
         var previous: UILabel?
         
         for label in [label1, label2, label3, label4, label5] {
-            //label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor).isActive = true
+//            label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
             
             if let previous = previous {
                 label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
